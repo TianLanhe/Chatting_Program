8adbd05e0b53119ccace203cd481ff492394ebf6
振宇 <849416763@qq.com>
2016-08-08 20:40:39 +0800

    客户端

--- chat_client.c
+++ chat_client.c
@@ -5,11 +5,11 @@
 #include <sys/msg.h>
 #include <error.h>
 #include <string.h>
-#define LOGIN 100
-#define SEND 90
-#define USER_LOG 1
-#define USER_OLOG 0
-#define KEY ((key_t)100)
+#define LOGIN 100				//message type of logining or out of login
+#define SEND 90					//message type of sending a message
+#define USER_LOG 1 				//the flag of logining
+#define USER_OLOG 0 			//the flag of out of logining
+#define KEY ((key_t)100) 		//id of message box
 typedef struct _log{
 	long mtype;
 	int pid;
@@ -25,15 +25,10 @@ typedef struct _chat{
 	char str[100];
 }Chat;
 char name[11];
+int mid;
 int login(){
-	int mid;
 	int sign;
 	Log message;
-	mid=msgget(LOGIN,IPC_CREAT|0660);
-	if(mid == -1){
-		perror("client creat message:");
-		return -1;
-	}
 	message.mtype=LOGIN;
 	message.pid=getpid();
 	message.flag=USER_LOG;
@@ -52,14 +47,8 @@ int login(){
 	return 0;
 }
 int Write_to(){
-	int mid;
 	int sign;
 	Chat message;
-	mid=msgget(KEY,IPC_CREAT|0660);
-	if(mid == -1){
-		perror("client creat message:");
-		return -1;
-	}
 	while(1){
 		printf("I:");
 		fgets(message.str,100,stdin);
@@ -88,39 +77,40 @@ int Write_to(){
 	}
 }
 int Read_from(){
-	int mid;
 	int sign;
 	int i;
 	Chat message;
-	mid=msgget(LOGIN,IPC_CREAT|0660);
-	if(mid == -1){
-		perror("client creat message:");
-		return -1;
-	}
 	while(1){
 		sign=msgrcv(mid,&message,sizeof(Chat)-sizeof(long),getpid(),0);
+		if(sign == -1){
+			perror("client receive:");
+			return -1;
+		}
 		if(message.pid_to == -1)printf("对不起，该用户已离开！\nI:");
 		else if(message.pid_to == -2)printf("对不起，格式错误！\nI:");
-		else  if(message.pid_to == getpid()){
+		else if(message.pid_to == getpid()){
 			for(i = 0; i < strlen("I:"); i++)printf("\b");
-			if(sign == -1){
-				perror("client receive:");
-				return -1;
-			}
 			printf("%s:%s\nI:",message.name_from,message.str);
 		}
 		fflush(stdout);
 	}
 }
 int main(){
-	int mid;
 	pthread_t thIDr, thIDw;
+
+	mid=msgget(KEY,IPC_CREAT|0660);
+	if(mid == -1){
+		perror("client creat message:");
+		return -1;
+	}
+	
 	if(login() == -1){
 		printf("Login Failed!\n");
 		return 1;
 	}
 	printf("Login Success!\n");
 	printf("My name is :%s\n", name);
+
 	pthread_create(&thIDr, NULL,(void *)Write_to, NULL);
 	pthread_create(&thIDw, NULL,(void *)Read_from, NULL);
 	pthread_join(thIDr, NULL);
